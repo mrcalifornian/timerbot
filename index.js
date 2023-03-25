ee2b1d5d1ab609186f85b173ed37b5f48cad3909
Nodirbek Tojiboev <nodirbek_tojiboev@mail.ru>
2023-03-25 10:20:37 +0500

    send message half and midnight

---
+++ index.js
@@ -0,0 +1,33 @@
+const TelegramBot = require('node-telegram-bot-api');
+require('dotenv').config();
+
+const TOKEN = process.env.TOKEN;
+const USER = process.env.USER;
+
+const bot = new TelegramBot(TOKEN, { polling: true });
+
+bot.on('text', body => {
+    let now = new Date();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+
+    if (body.from.id == USER) {
+        bot.sendMessage(USER, `${hour} : ${minute} is current time!`)
+    }
+})
+
+let sendHourly = () => {
+    let now = new Date();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+
+    if (hour == 0 && minute == 0) {
+        bot.sendMessage(USER, `${hour}:${minute}\nThis was supposed to be sent at midnight!`)
+    }
+
+    if (minute == 30) {
+        bot.sendMessage(USER, `${hour}:${minute}\nThis was supposed to be sent at half of every hour!`)
+    }
+}
+
+setInterval(sendHourly, 60000);
